dbf2fe4d74bba01ac0710bc7f0c1b21caf553ac0
CaioMicael <142358332+CaioMicael@users.noreply.github.com>
2023-10-08 08:59:46 -0300

    teste

---
+++ viagem.js
@@ -0,0 +1,8 @@
+function CalculaViagem() {
+    let LocalViagem = document.getElementById('iViagem').value;
+    let KM = parseFloat(document.getElementById('iKMPercorridos').value);   
+    let LitrosCombustivel = parseFloat(document.getElementById('iLitrosCombustivel').value);
+    let ValorLitro = parseFloat(document.getElementById('iValorLitro').value);
+    let resultado = '';
+    
+}
